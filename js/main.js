da00f5fc954db053485d0615b8dcaddf683d6b2b
Carl J. Bialorucki <cbialo2@outlook.com>
2022-09-17 14:02:31 -0600

    Added home page; week 1 content.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const pages = [["Week 1: Building a Portfolio", "week1"]];
+let list = document.getElementsByTagName("ol")[0];
+pages.forEach(addPageToList);
+
+function addPageToList(item){
+    let newItem = document.createElement("li");
+    let newItemLink = document.createElement("a");
+    newItemLink.href = item[1];
+    newItemLink.innerText = item[0];
+    newItem.appendChild(newItemLink);
+    list.appendChild(newItem);
+}
