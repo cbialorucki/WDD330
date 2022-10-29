95ff3c80dfd8593dc4d1911557203a4df0bea17a
Ticomware <cbialo2@outlook.com>
2022-10-29 14:57:05 -0600

    Added Week 7 Notes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const pages = [["Week 1: Building a Portfolio", "week1"], ["Week 2: Programming Basics", "week2"], 
 ["Week 3: Objects, The DOM, and Events", "week3"], ["Week 4: Forms, OOP, and Modular JavaScript", "week4"],
-["Week 5: Testing and Debugging", "week5"], ["Week 6: Todo Web App", "week6/todo"]];
+["Week 5: Testing and Debugging", "week5"], ["Week 6: Todo Web App", "week6/todo"],
+["Week 7: Further Functions and AJAX", "week7"]];
+
 let list = document.getElementsByTagName("ol")[0];
 pages.forEach(addPageToList);
 
